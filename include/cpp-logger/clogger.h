c271a09ee0060bd68047a2d4eda94a072669b658
Hariharan Devarajan <mani.hariharan@gmail.com>
2022-11-30 15:34:49 -0800

    added c function to set log level.

--- include/cpp-logger/clogger.h
+++ include/cpp-logger/clogger.h
@@ -1,6 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
 const int CPP_LOGGER_PRINT = 1;
 const int CPP_LOGGER_ERROR = 2;
 const int CPP_LOGGER_WARN = 3;
 const int CPP_LOGGER_INFO = 4;
-extern void clog(const int logger_level, const char* name, const char* string,
+extern void cpp_logger_clog(const int logger_level, const char* name, const char* string,
                  ...);
+extern void cpp_logger_clog_level(const int logger_level, const char* name);
+#ifdef __cplusplus
+}
+#endif
--- src/cpp-logger/clogger.cpp
+++ src/cpp-logger/clogger.cpp
@@ -1,10 +1,13 @@
 #include <cpp-logger/clogger.h>
 #include <cpp-logger/logger.h>
 
-void clog(int logger_level, const char *name, const char *string, ...) {
+void cpp_logger_clog(int logger_level, const char *name, const char *string, ...) {
   va_list args;
   va_start(args, string);
   cpplogger::Logger::Instance(name)->log(
       static_cast<cpplogger::LoggerType>(logger_level), string, args);
   va_end(args);
+}
+void cpp_logger_clog_level(const int logger_level, const char *name) {
+  cpplogger::Logger::Instance(name)->level = static_cast<cpplogger::LoggerType>(logger_level);
 }
