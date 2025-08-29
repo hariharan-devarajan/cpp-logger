1338e32983aadde8d1a88d0bb1daee1938227593
Ray Andrew <rs@rs.ht>
2025-08-28 21:58:14 -0700

    add file parameter

--- include/cpp-logger/logger.h
+++ include/cpp-logger/logger.h
@@ -25,12 +25,13 @@ class Logger {
  private:
   static std::unordered_map<std::string, std::shared_ptr<Logger>> instance_map;
   std::string _app_name;
+  FILE *_file;
 
  public:
   LoggerType level;
 
-  explicit Logger(std::string app_name)
-      : _app_name(app_name), level(LoggerType::LOG_ERROR) {}
+  explicit Logger(std::string app_name, FILE *file = stdout)
+      : _app_name(app_name), level(LoggerType::LOG_ERROR), _file(file) {}
 
   static std::shared_ptr<Logger> Instance(std::string app_name = "LOGGER") {
     auto iter = instance_map.find(app_name);
@@ -53,43 +54,43 @@ class Logger {
     switch (type) {
       case LoggerType::LOG_PRINT: {
         if (level >= LoggerType::LOG_PRINT) {
-          fprintf(stdout, "[%s PRINT]: %s\n", _app_name.c_str(), buffer);
-          fflush(stdout);
+          fprintf(_file, "[%s PRINT]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
       case LoggerType::LOG_TRACE: {
         if (level >= LoggerType::LOG_TRACE) {
-          fprintf(stdout, "[%s TRACE]: %s\n", _app_name.c_str(), buffer);
-          fflush(stdout);
+          fprintf(_file, "[%s TRACE]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
       case LoggerType::LOG_DEBUG: {
         if (level >= LoggerType::LOG_DEBUG) {
-          fprintf(stdout, "[%s DEBUG]: %s\n", _app_name.c_str(), buffer);
-          fflush(stdout);
+          fprintf(_file, "[%s DEBUG]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
       case LoggerType::LOG_INFO: {
         if (level >= LoggerType::LOG_INFO) {
-          fprintf(stdout, "[%s INFO]: %s\n", _app_name.c_str(), buffer);
-          fflush(stdout);
+          fprintf(_file, "[%s INFO]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
       case LoggerType::LOG_WARN: {
         if (level >= LoggerType::LOG_WARN) {
-          fprintf(stdout, "[%s WARN]: %s\n", _app_name.c_str(), buffer);
-          fflush(stdout);
+          fprintf(_file, "[%s WARN]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
       case LoggerType::LOG_ERROR: {
         if (level >= LoggerType::LOG_ERROR) {
-          fprintf(stderr, "[%s ERROR]: %s\n", _app_name.c_str(), buffer);
-          fflush(stderr);
+          fprintf(_file, "[%s ERROR]: %s\n", _app_name.c_str(), buffer);
+          fflush(_file);
         }
         break;
       }
